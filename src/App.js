b7591c3e3c54dddfb8de18f00cf37d505697ef9d
Erica Glimsholt <ericaglimsholt@hotmail.com>
2018-01-28 13:29:34 +0100

    Added component usp and changed typograph settings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.scss';
 import ReactGA from 'react-ga';
-import Footer from './components/Footer';
-import AddTodo from './containers/AddTodo';
-import VisibleTodoList from './containers/VisibleTodoList';
+
+import Usp from './components/Usp';
 
 export default class App extends Component {
     constructor() {
@@ -19,10 +18,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <h1>Idrottskoll</h1>
-                <AddTodo />
-                <VisibleTodoList />
-                <Footer />
+                <Usp />
             </div>
         );
     }
---
+++ src/components/Usp.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+
+export default class Usp extends Component {
+
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col md={4}>
+            <div className="bg-light-grey bp-20">
+              <Image src="src/images/icons/add-order-icon--black.svg" />
+              <h3 className="center">1. Lägg order</h3>
+              <p className="center rm-20 lm-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="bg-light-grey bp-20">
+              <Image src="src/images/icons/film-movie-icon--black.svg" />
+              <h3 className="center">2. Videon filmas</h3>
+              <p className="center rm-20 lm-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="bg-light-grey bp-20">
+              <Image src="src/images/icons/watch-movie-icon--black.svg" />
+              <h3 className="center">3. Titta på videon</h3>
+              <p className="center rm-20 lm-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
