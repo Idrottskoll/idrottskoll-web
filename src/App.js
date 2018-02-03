2d98c4a18e012e0a20e8494ae6d60c67828f6892
Jeremy Danenr <jeremydanner2@gmail.com>
2018-02-03 13:36:57 +0100

    added sign in and sign out button

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
+                {!localStorage.token ? <SigninModal /> : <SignoutButton />}
                 <Usp />
                 <About />
                 <CaseSlider />
