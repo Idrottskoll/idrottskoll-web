9f655487c64ec863db68b746c13f2e78ada6996e
Jeremy Danner <jeremydanner2@gmail.com>
2018-02-06 20:35:07 +0100

    fetching live strems

--- src/components/video/Streams.js
+++ src/components/video/Streams.js
@@ -4,16 +4,37 @@ import axios from 'axios';
 export default class Streams extends React.Component {
     constructor() {
         super();
+        this.state = { streams: false };
     }
 
-    fetchStreams = async () => {
-        return false;
+    fetchStreams = () => {
+        axios
+            .get('https://www.ikoll.se/api/v1/orders')
+            .then(response => {
+                if (response.data) {
+                    return this.setState({ streams: response.data });
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
     };
 
+    displayStreams() {
+        return this.state.streams.map(stream => {
+            if (stream.record) {
+                return false;
+            }
+
+            return <div key={stream._id}>Live nu: {stream.stream.club}!</div>;
+        });
+    }
+
     componentWillMount = async () => {
-        const streams = await this.fetchStreams();
+        this.fetchStreams();
     };
+
     render() {
-        return <div>Live</div>;
+        return this.state.streams !== false ? this.displayStreams() : null;
     }
 }
